c1ab758f411fff402d340667d28f23ab1002bc9d
P0lcahontas <pokhariyal@usiu.ac.ke>
2024-11-13 16:25:15 +0300

    First Commit!

---
+++ src/main/java/com/example/SWE2040_Project/Swe2040ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.SWE2040_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Swe2040ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Swe2040ProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SWE2040_Project/Swe2040ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SWE2040_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Swe2040ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
