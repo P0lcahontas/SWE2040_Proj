a51f67ba1e562e2b8602e5b60fbfc37d3a9a5f16
P0lcahontas <pokhariyal@usiu.ac.ke>
2024-11-16 20:57:46 +0300

    Some stuff and started on the javafx

--- src/main/java/ChatController.java
+++ src/main/java/ChatController.java
@@ -1,3 +1,15 @@
+
+import com.example.SWE2040_Project.ChatMessagePojo;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -7,6 +19,27 @@
  *
  * @author anantpokhariyal
  */
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class ChatController {
-    
+
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/public")
+    public ChatMessagePojo sendMessage(@Payload ChatMessagePojo chatMessagePojo) {
+        return chatMessagePojo;
+    }
+
+    @MessageMapping("/chat.addUser")
+    @SendTo("/topic/public")
+    public ChatMessagePojo addUser(@Payload ChatMessagePojo chatMessagePojo, SimpMessageHeaderAccessor headerAccessor) {
+        
+// Add username in web socket session
+    headerAccessor.getSessionAttributes().put("username", chatMessagePojo.getSender());
+        return chatMessagePojo;
+    }
 }
---
+++ src/main/java/com/example/SWE2040_Project/WebSocketEventListener.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.SWE2040_Project;
+
+/**
+ *
+ * @author anantpokhariyal
+ */
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionConnectedEvent;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+import org.slf4j.Logger;
+
+@Component
+public class WebSocketEventListener {
+
+    private static final Logger logger = LoggerFactory.getLogger(WebSocketEventListener.class);
+
+    @Autowired
+    private SimpMessageSendingOperations messagingTemplate;
+
+    @EventListener
+    public void handleWebSocketConnectListener(SessionConnectedEvent event) {
+        logger.info("Received a new web socket connection");
+    }
+
+    @EventListener
+    public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+
+        String username = (String) headerAccessor.getSessionAttributes().get("username");
+        if(username != null) {
+            logger.info("User Disconnected : " + username);
+
+            ChatMessagePojo chatMessagePojo = new ChatMessagePojo();
+            chatMessagePojo.setType(ChatMessagePojo.MessageType.LEAVE);
+            chatMessagePojo.setSender(username);
+
+            messagingTemplate.convertAndSend("/topic/public", chatMessagePojo);
+        }
+    }
+}
